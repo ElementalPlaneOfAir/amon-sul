59df098e4cf684d877dd40a6860918b40df6fad0
Nicole Venner <nvenner@protonmail.ch>
2026-06-13 08:22:47 -0600

    add tailscale update

--- machines/amon-sul/configuration.nix
+++ machines/amon-sul/configuration.nix
@@ -9,6 +9,7 @@
   ];
   nixpkgs.config.permittedInsecurePackages = ["olm-3.2.16"];
   networking.hostName = "amon-sul";
+  services.tailscale.enable = true;
 
   # Clan deployment target
   clan.core.networking.targetHost = "root@192.168.0.7";
